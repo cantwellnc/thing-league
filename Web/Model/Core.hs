d42d1db200459b896462936f66064db587a9f4cc
cantwellnc <danielcantwell@Daniels-MacBook-Pro.local>
2026-09-07 21:58:54 -0400

    - more data modeling, simplifying transitions and constructors for round

--- Web/Model/Core.hs
+++ Web/Model/Core.hs
@@ -1,88 +1,163 @@
+
 module Web.Model.Core where
+
 import IHP.Prelude
 
-newtype LeagueId = LeagueId UUID
-newtype UserId = UserId UUID
-newtype RoundId = RoundId UUID
-newtype SubmissionId = SubmissionId UUID
-newtype VoteId = VoteId UUID
+-- TODO (claude): once this connects to IHP persistence, consider replacing these with
+-- IHP.ModelSupport's Id' pattern (newtype Id' table = Id (PrimaryKey table);
+-- type Id model = Id' (GetTableName model)) instead of one newtype per id.
+newtype LeagueId = LeagueId UUID deriving (Eq, Show)
+newtype UserId = UserId UUID deriving (Eq, Show)
+newtype RoundId = RoundId UUID deriving (Eq, Show)
+newtype SubmissionId = SubmissionId UUID deriving (Eq, Show)
+newtype VoteId = VoteId UUID deriving (Eq, Show)
 data MemberId = MemberId UUID UUID -- league + user
 
+type Ranking = [(Submission, Int)] -- (submission, # of points it got)
+data LeagueState
+    = LeagueOpen
+    | LeagueInProgress
+    | LeagueComplete
+    deriving (Eq, Show)
 
-data LeagueState = LeagueOpen | LeagueInProgress | LeagueComplete deriving (Eq, Show)
-data RoundState = Open | Voting | Complete deriving (Eq, Show)
+data Round
+    = OpenedRound
+        { roundId :: RoundId
+        , roundLeague :: LeagueId
+        , roundTheme :: Text
+        , mediaSubmitDeadline :: UTCTime
+        }
+    | VotingRound
+        { roundId :: RoundId
+        , roundLeague :: LeagueId
+        , roundTheme :: Text
+        , voteSubmitDeadline :: UTCTime
+        , voteBudget :: Int
+        , maxVotesPerSubmission :: Int
+        }
+    | CompletedRound
+        { roundId :: RoundId
+        , roundLeague :: LeagueId
+        , roundTheme :: Text
+        , ranking :: Ranking
+        }
+    deriving Show
+    
 
-data Round = Round {
-    roundId               :: RoundId,
-    roundLeague           :: LeagueId,
-    roundTheme            :: Text,
-    roundState            :: RoundState,
-    roundSubmitDeadline   :: UTCTime,
-    roundBudget           :: Int,
-    roundMaxPerSubmission :: Int
-}
+data RoundCommand
+    = StartRound
+    | OpenVoting
+    | CompleteRound
 
-data User = User {
-    id          :: UserId, 
-    displayName :: Text, 
-    email       :: Text, 
-    createdAt   :: UTCTime
-}
-
-data League = League {
-    id        :: LeagueId,
-    name      :: Text,
-    createdBy :: UserId,
-    createdAt :: UTCTime
-}
-
-data LeagueMember = LeagueMember {
-    leagueId :: LeagueId, 
-    userId   :: UserId, 
-    joinedAt :: UTCTime
-}
-
-data Submission = Submission {
-    id          :: SubmissionId, 
-    url         :: Text, 
-    comment     :: Text, 
-    submittedAt :: UTCTime
-}
-
-data Vote = Vote {
-    id      :: VoteId, 
-    roundId :: RoundId,
-    voterId :: UserId, 
-    points  :: Int
-}
-
+data RoundEvent
+    = RoundStarted
+    | VotingOpened
+        { voteSubmitDeadline :: UTCTime
+        , voteBudget :: Int
+        , maxVotesPerSubmission :: Int
+        }
+    | RoundCompleted Ranking
+    deriving Show
 
 {-
-flow: 
+work of summing events, getting data that event processor needs from submissions / rounds, etc.
+is packed into the place running processRoundCommand. emits events are "record" of what transpired.
+-}
+
+-- have to provide initial constructor for first event
+startRound :: RoundId -> LeagueId -> Text -> UTCTime -> Round
+startRound roundId leagueId theme submitDeadline 
+    = OpenedRound {
+        roundId = roundId, 
+        roundLeague = leagueId, 
+        roundTheme = theme, 
+        mediaSubmitDeadline = submitDeadline
+    }
+
+processRoundEvent :: RoundEvent -> Round -> Either Text Round
+processRoundEvent (VotingOpened voteDeadline voteBudget maxVotes) (OpenedRound id league theme submitDeadline) =
+    Right $ VotingRound
+        { roundId = id
+        , roundLeague = league
+        , roundTheme = theme
+        , voteSubmitDeadline = submitDeadline
+        , voteBudget = voteBudget
+        , maxVotesPerSubmission = maxVotes
+        }
+processRoundEvent (RoundCompleted ranking) (VotingRound id league theme submitDeadline _ _) =
+    Right $ CompletedRound
+        { roundId = id
+        , roundLeague = league
+        , roundTheme = theme
+        , ranking = ranking
+        }
+processRoundEvent event round = Left $ "Illegal transition " <> (tshow event) <> " in round " <> (tshow round)
+
+-- WIP
+
+data User = User
+    { id :: UserId
+    , displayName :: Text
+    , email :: Text
+    , createdAt :: UTCTime
+    }
+
+data League = League
+    { id :: LeagueId
+    , name :: Text
+    , createdBy :: UserId
+    , createdAt :: UTCTime
+    }
+
+data LeagueMember = LeagueMember
+    { leagueId :: LeagueId
+    , userId :: UserId
+    , joinedAt :: UTCTime
+    }
+
+data Submission = Submission
+    { id :: SubmissionId
+    , submittedBy :: UserId
+    , roundId :: RoundId
+    , media :: Text
+    , comment :: Text
+    , submittedAt :: UTCTime
+    }
+    deriving Show
+
+data Vote = Vote
+    { id :: VoteId
+    , voterId :: UserId
+    , submissionId :: SubmissionId
+    , roundId :: RoundId
+    , points :: Int
+    }
+
+{-
+flow:
 
 create account if not exists
 create league if not exists
 generate link if not exists
 
 register for league if not registered
- 
+
 -- league begins
 -- round 1 begins
--- create submission if not exists 
+-- create submission if not exists
     -- possibly update existing submission
--- submission ends, voting begins 
--- create votes if not exists 
+-- submission ends, voting begins
+-- create votes if not exists
     -- possibly update existing votes
 
-
-What functionality do we need? 
+What functionality do we need?
 - CRUD user
 
 - CRUD league
 - join league as user
 - create invite for league
 - league transitions
-- results for league 
+- results for league
 
 - Round transitions
 - create playlist of media for Round
@@ -92,18 +167,12 @@ What functionality do we need?
 -}
 
 data LeagueEvent = LeagueStarted | LeagueFinished
-data RoundEvent = RoundStarted | SubmissionsOpened | SubmissionsClosed | VotingOpened | VotingClosed | RoundEnded
 
 -- placeholders
 data LeagueCommand = StartLeague | EndLeague
-data RoundCommand = StartRound |  OpenSubmissions | CloseSubmissions | OpenVoting | CloseVoting | EndRound
-
 
 processLeagueEvent :: LeagueEvent -> League -> League
 processLeagueEvent event league = undefined
-
-processRoundEvent :: RoundEvent -> Round -> Round
-processRoundEvent event round = undefined
 
 processLeagueCommand :: LeagueCommand -> League -> League
 processLeagueCommand command league = undefined
